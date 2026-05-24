97b7990e5bf4e74c2f394a3cd923bab88ac3a8e6
unknown <megantntran@gmail.com>
2026-05-24 09:48:18 -0400

    fix titles

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Strings Attached Blog';
+export const SITE_TITLE = 'Strings Attached';
 export const SITE_DESCRIPTION = 'Documentation and Additional Notes';
